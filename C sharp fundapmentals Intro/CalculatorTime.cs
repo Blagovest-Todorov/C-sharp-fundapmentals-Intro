c8515b89f3c30103d5c86dfebfa4d4a3147ba964
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-01-06 11:49:34 +0200

    Add project files.

---
+++ C sharp fundapmentals Intro/CalculatorTime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace timeCalulatorNewSolution
+{
+    class CalculatorTime
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            minutes += 30;
+            if (hour < 0 || minutes < 0)
+            {
+                Console.WriteLine("Hour and minutes needs to be only positive numbers !");
+            }
+            else
+            {
+                if (minutes >= 60)
+                {
+
+                    hour++;
+                    minutes = minutes - 60;
+                }
+                if (hour > 23)
+                {
+                    hour = 0;
+                }
+                Console.WriteLine($"{hour} h : {minutes} m");
+            }
+
+
+        }
+    }
+}
